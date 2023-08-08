f0cb50edb15dcdf70208f1038ff73aab39c23e04
Julius Babies <julius@familie-babies.de>
2023-08-08 20:22:45 +0200

    base

---
+++ app/src/main/java/de/babiesjulius/navtest/HomeFragment.kt
@@ -0,0 +1,30 @@
+package de.babiesjulius.navtest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import de.babiesjulius.navtest.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.binding.btnOpenDevices.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_deviceFragment)
+        }
+    }
+}
